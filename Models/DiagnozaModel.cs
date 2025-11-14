9905058655b087c0e0108f034c839bdf16c035fa
Rubin1977 <ruzbacky@yahoo.com>
2025-11-14 19:42:18 +0100

    Fix: PostgreSQL podpora, render.yaml connection string, upravený DiagnozaModel

--- Models/DiagnozaModel.cs
+++ Models/DiagnozaModel.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ZivotopisCore.Models
 {
@@ -9,22 +10,23 @@ namespace ZivotopisCore.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Kód diagnózy je povinný.")]
+        [Column(TypeName = "text")]
         public string? Kod { get; set; } // napr. ICD-10
+
+        [Column(TypeName = "text")]
         public required string Nazov { get; set; }
 
+        [Column(TypeName = "text")]
         public string? Popis { get; set; }
+
+        [Column(TypeName = "text")]
         public string? Typ { get; set; } // napr. chronická, akútna
 
         public bool Aktivna { get; set; } = true;
+
         public DateTime DátumVytvorenia { get; set; } = DateTime.Now;
 
         // Navigačná vlastnosť pre Many-to-Many
         public ICollection<PacientModel> Pacienti { get; set; } = new List<PacientModel>();
-
-
-
-
-
     }
-
 }
