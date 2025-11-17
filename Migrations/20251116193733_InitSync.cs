5d9eff0f219d84a69df668f91528387d11c34c33
Rubin1977 <ruzbacky@yahoo.com>
2025-11-17 13:42:12 +0100

    Update views, CSS, add InitSync migration, remove old InitialPostgres migration

---
+++ Migrations/20251116193733_InitSync.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZivotopisCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitSync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // baseline – nič nevytvára
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // baseline – nič nemaže
+        }
+    }
+}
